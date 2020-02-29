19ec65cf661c2f2c42b774e9072309fc3bd86293
boris.barac <boris.barac@massive.co>
2020-02-29 14:45:32 +0100

    Initial Commit

---
+++ Music search/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Music search
+//
+//  Created by Boris Barac on 29/02/2020.
+//  Copyright © 2020 Boris Barac. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
