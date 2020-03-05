f2a27ff37c319a8085690e1e336a377ef71c4e0a
boris.barac <boris.barac@massive.co>
2020-03-05 23:28:07 +0100

    NetworkManager changes part 1

--- Music search/Networking/NetworkingManager.swift
+++ Music search/Networking/NetworkingManager.swift
@@ -7,25 +7,14 @@
 //
 
 import Foundation
+import Combine
 
 extension NSNotification.Name {
     static let mainReload = Notification.Name(rawValue: "main_reload")
 }
 
 final class NetworkManager {
-    enum EndPoints: String {
-        case main = "https://itunes.apple.com/search"
-
-        var url: URL! {
-            return URL(string: self.rawValue)
-        }
-    }
-
-    enum NetworkError: Error {
-        case didNotWork
-    }
-
-    private let urlSession = URLSession.shared
+    private let agent = Agent()
 
     func buildSearchUrl(text: String) -> URL? {
         guard let base = EndPoints.main.url else {
@@ -39,23 +28,56 @@ final class NetworkManager {
         return components?.url
     }
 
-    func getJson(url: URL, completionHandler: @escaping (Result<MainJson, NetworkError>) -> Void) {
-        let task = urlSession.dataTask(with: url){ (data, response, error) in
-            debugPrint(response as Any)
-            guard let data = data else {
-                completionHandler(.failure(.didNotWork))
-                return
-            }
+    func getJson(url: URL) -> AnyPublisher<MainJson, Error> {
+        return run(URLRequest(url: url))
+    }
 
-            do {
-                let blogPost: MainJson = try JSONDecoder().decode(MainJson.self, from: data)
-                completionHandler(.success(blogPost))
-                debugPrint(blogPost)
-            } catch {
-                debugPrint("parseFailed")
-                completionHandler(.failure(.didNotWork))
-            }
+    func run<T: Decodable>(_ request: URLRequest) -> AnyPublisher<T, Error> {
+        return agent.run(request)
+            .map(\.value)
+            .retry(1)
+            .eraseToAnyPublisher()
+    }
+}
+
+// MARK: - Inline enums
+extension NetworkManager {
+    enum EndPoints: String {
+        case main = "https://itunes.apple.com/search"
+
+        var url: URL! {
+            return URL(string: self.rawValue)
         }
-        task.resume()
+    }
+
+    enum NetworkError: Error {
+        case didNotWork(error: Error)
+    }
+}
+
+// MARK: - Agent struct
+extension NetworkManager {
+    struct Agent {
+        let session = URLSession.shared
+
+        struct Response<T> {
+            let value: T
+            let response: URLResponse
+        }
+
+        func run<T: Decodable>(_ request: URLRequest,
+                               _ decoder: JSONDecoder = JSONDecoder()) -> AnyPublisher<Response<T>, Error> {
+            return session
+                .dataTaskPublisher(for: request)
+//                .mapError({ err -> Error in
+//                    return NetworkManager.NetworkError.didNotWork(error: err)
+//                })
+                .tryMap { result -> Response<T> in
+                    let value = try decoder.decode(T.self, from: result.data)
+                    return Response(value: value, response: result.response)
+                }
+                .receive(on: DispatchQueue.main)
+                .eraseToAnyPublisher()
+        }
     }
 }
